d70488268621e514c4b9e1e39a8c86df14ba2af2
Ivan Maltese <ivan.maltese@objectmethod.it>
2019-11-20 17:03:21 +0100

    improvements

--- src/main/java/id/objectmethod/relazioni/domain/Padre.java
+++ src/main/java/id/objectmethod/relazioni/domain/Padre.java
@@ -15,35 +15,42 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="padre")
+@Table(name = "padre")
 public class Padre {
 
 	@GeneratedValue
 	@Id
 	private Long id;
-	
-	@Column(name="nome")
+
+	@Column(name = "nome")
 	private String nome;
-	
-	@Column(name="cognome")
+
+	@Column(name = "cognome")
 	private String cognome;
-	
-	@Column(name="eta")
+
+	@Column(name = "eta")
 	private Integer eta;
-	
-	@JoinColumn(name="padre_id") // indichiamo che nella tabella figlio la colonna padre_id fa riferimento all'id di questa entita
-	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL) // OneToMany è il tipo di relazione
-	//Fetch indica quando vengono estratti da DB le entita collegate
-	//LAZY le estrae solo quando chiamiamo il relativo metodo GET
-	//EAGER le estrae subito nel momento in cui estraiamo il padre
-	//Cascade indica se un operazione di modifica/eliminazione fatta nel padre si deve ripercuotere anche sulle entita collegate
+
+//	@Column(name = "altezzona")
+//	private Integer altezza;
+
+	@JoinColumn(name = "padre_id") // indichiamo che nella tabella figlio la colonna padre_id fa riferimento all'id
+									// di questa entita
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // OneToMany è il tipo di relazione
+	// Fetch indica quando vengono estratti da DB le entita collegate
+	// LAZY le estrae solo quando chiamiamo il relativo metodo GET
+	// EAGER le estrae subito nel momento in cui estraiamo il padre
+	// Cascade indica se un operazione di modifica/eliminazione fatta nel padre si
+	// deve ripercuotere anche sulle entita collegate
 	private List<Figlio> figli;
-	
+
 	@ManyToMany
-	@JoinTable(name = "padri_auto", joinColumns = @JoinColumn(name = "padre_id", referencedColumnName = "id")
-	, inverseJoinColumns = @JoinColumn(name = "auto_id", referencedColumnName = "id"))
-	//padri_auto tabella di correlazione tra padre e automobile, nelle joinColumns indichiamo che padre_id (della tabella padri_auto) fa riferimo a id (della tabella padre)
-	//mentre in inverseJoinColumns indichiamo che auto_id (della tabella padri_auto) fa riferimento a id (della tabella automobile).
+	@JoinTable(name = "padri_auto", joinColumns = @JoinColumn(name = "padre_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "auto_id", referencedColumnName = "id"))
+	// padri_auto tabella di correlazione tra padre e automobile, nelle joinColumns
+	// indichiamo che padre_id (della tabella padri_auto) fa riferimo a id (della
+	// tabella padre)
+	// mentre in inverseJoinColumns indichiamo che auto_id (della tabella
+	// padri_auto) fa riferimento a id (della tabella automobile).
 	private List<Automobile> automobili;
 
 	public Long getId() {
@@ -69,7 +76,7 @@ public class Padre {
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
-	
+
 	public Integer getEta() {
 		return eta;
 	}
@@ -93,5 +100,13 @@ public class Padre {
 	public void setAutomobili(List<Automobile> automobili) {
 		this.automobili = automobili;
 	}
+
+//	public Integer getAltezza() {
+//		return altezza;
+//	}
+//
+//	public void setAltezza(Integer altezza) {
+//		this.altezza = altezza;
+//	}
 
 }
--- src/main/java/id/objectmethod/relazioni/repo/PadreRepository.java
+++ src/main/java/id/objectmethod/relazioni/repo/PadreRepository.java
@@ -1,7 +1,6 @@
 package id.objectmethod.relazioni.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import id.objectmethod.relazioni.domain.Padre;
@@ -9,7 +8,7 @@ import id.objectmethod.relazioni.domain.Padre;
 @Repository
 public interface PadreRepository extends JpaRepository<Padre, Long>{
 	
-	@Query("select p from Padre p join Figlio f on p.id = f.padre.id where f.id = ?1") //notare bene la join soprattutto su figlio, dove usiamo f.padre.id
-	public Padre getPadreByIdFiglio(Long idFiglio);
+//	@Query("select p from Padre p join Figlio f on p.id = f.padre.id where f.id = ?1") //notare bene la join soprattutto su figlio, dove usiamo f.padre.id
+//	public Padre getPadreByIdFiglio(Long idFiglio);
 	
 }
